955eb64061eee71ff2d7354987be0b112d534713
7-Hikari <rafliramadanix@gmail.com>
2026-04-19 23:39:42 +0700

    aneh, kok kehapus ya tapi masih jalan loh

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\autentikasi;
 use App\Http\Controllers\crud_penyakit;
 use App\Http\Controllers\dashboard;
 use App\Http\Controllers\ManajemenUserController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
